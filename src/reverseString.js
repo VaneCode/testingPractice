def5943128573cf834a0e71938a33571a72bc7f3
vanessaBonito <vanebonito@hotmail.com>
2022-07-19 14:17:34 -0500

    Add function reverseString

---
+++ src/reverseString.js
@@ -0,0 +1,3 @@
+reverseString(string){
+    
+}
--- src/stringLengthTest.js
+++ src/stringLengthTest.js
@@ -3,3 +3,12 @@ const stringLength = require('./stringLength');
 test('string "word" should return 4', () => {
     expect(stringLength('word')).toBe(4);
   });
+
+  test('string "dictionaries" has more than 10 characteres, it should return Error', () => {
+    expect(stringLength('dictionaries')).toBe('Error');
+  });  
+
+  test('string "" its empty, it should return Error', () => {
+    expect(stringLength('')).toBe('Error');
+  });  
+
