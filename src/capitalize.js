aecf2292dde19b9f5e1b18f17a5115f7dd3a12db
vanessaBonito <vanebonito@hotmail.com>
2022-07-19 15:52:02 -0500

    Update test and function capitalize

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,5 @@
 const capitalize = (string) => {
+    if(string !== '' && typeof(string) === 'string')
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -3,3 +3,11 @@ const capitalize = require('./capitalize');
 test('string "dog" should return Dog' , () => {
   expect(capitalize('dog')).toBe('Dog');
 });
+
+test('empty string should return "Error"' , () => {
+  expect(capitalize('')).toBe('Error');
+});
+
+test('true should return "Error"' , () => {
+    expect(capitalize(true)).toBe('Error');
+  });
